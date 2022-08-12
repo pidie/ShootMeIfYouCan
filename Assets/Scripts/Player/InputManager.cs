4944b677cbe852eb35dd66e06f841f2785c56d64
Matt Burns <mmburns88@gmail.com>
2022-08-12 00:13:31 -0400

    camera will not move when pause menu is open

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -1,18 +1,26 @@
+using System;
+using Cinemachine;
 using Guns;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private GunBehavior gun;
+        [SerializeField] private GameObject pauseMenu;
+        [SerializeField] private CinemachineInputProvider inputProvider;
+        [SerializeField] private InputActionReference playerXYAxis;
 
         private PlayerControls _playerControls;
         private bool _reportAccurateDelta;
+        public bool DoNotMoveCamera { get; private set; }
 
         public bool IsPlayerDead { get; set; }
 
         public static InputManager Instance { get; private set; }
+        public static Action<bool> onDoNotMoveCamera;
 
         private void Awake()
         {
@@ -24,7 +32,12 @@ namespace Player
             _playerControls = new PlayerControls();
 
             _playerControls.PlayerGeneric.Jump.performed += _ => PlayerMovement.jumpCallback.Invoke();
-            _playerControls.PlayerGeneric.Fire.performed += _ => gun.Fire(IsPlayerDead);
+            _playerControls.PlayerGeneric.Fire.performed += _ => {
+                if (!DoNotMoveCamera)
+                    gun.Fire(IsPlayerDead);
+            };
+            _playerControls.PlayerGeneric.MenuScreen.performed += _ => pauseMenu.SetActive(!pauseMenu.activeSelf);
+            onDoNotMoveCamera += DisableCameraMotion;
         }
 
         private void OnEnable() => _playerControls.Enable();
@@ -35,10 +48,16 @@ namespace Player
 
         public Vector2 GetMouseDelta()
         {
-            if (IsPlayerDead) return Vector2.zero;
+            if (IsPlayerDead || DoNotMoveCamera) return Vector2.zero;
             return _reportAccurateDelta ? _playerControls.PlayerGeneric.Look.ReadValue<Vector2>() : Vector2.zero;
         }
 
         public bool PlayerJumpedThisFrame() => _playerControls.PlayerGeneric.Jump.WasPressedThisFrame();
+
+        private void DisableCameraMotion(bool value)
+        {
+            DoNotMoveCamera = value;
+            inputProvider.XYAxis = value ? null : playerXYAxis;
+        }
     }
 }
