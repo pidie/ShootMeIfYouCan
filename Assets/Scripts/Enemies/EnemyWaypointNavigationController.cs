6fc81166984c206d16049e72cccef4590cfc26d9
Matt Burns <mmburns88@gmail.com>
2022-10-27 12:21:35 -0400

    added controllers to move objects around waypoints

---
+++ Assets/Scripts/Enemies/EnemyWaypointNavigationController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Enemies
+{
+	/// <summary>
+	/// This class controls the automated movement of enemies through the waypoint matrix.
+	/// </summary>
+	public class EnemyWaypointNavigationController : MonoBehaviour
+	{
+		[SerializeField] private Vector3 destination;
+		[SerializeField] private float stopDistance;
+		[SerializeField] private float rotationSpeed;
+		[SerializeField] private Vector3 velocity;
+		[SerializeField] private Vector3 lastPosition;
+
+		private float movementSpeed;
+
+		public bool ReachedDestination { get; private set; }
+
+		private void Awake()
+		{
+			destination = Vector3.zero;
+			movementSpeed = GetComponent<EnemyBehavior>().GetMovementSpeed();
+		}
+
+		private void Update()
+		{
+			if (transform.position != destination)
+			{
+				var destinationDirection = destination - transform.position;
+
+				var destinationDistance = destinationDirection.magnitude;
+
+				if (destinationDistance >= stopDistance)
+				{
+					ReachedDestination = false;
+					var targetRotation = Quaternion.LookRotation(destinationDirection);
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation,
+						rotationSpeed * Time.deltaTime);
+				}
+				else ReachedDestination = true;
+
+				transform.position += destinationDirection.normalized * (movementSpeed * Time.deltaTime);
+			}
+		}
+
+		public void SetDestination(Vector3 destination)
+		{
+			this.destination = destination;
+			ReachedDestination = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Waypoints/WaypointNavigator.cs
@@ -0,0 +1,62 @@
+﻿using Enemies;
+using UnityEngine;
+
+namespace Waypoints
+{
+	public class WaypointNavigator : MonoBehaviour
+	{
+		private EnemyWaypointNavigationController controller;
+		[SerializeField] private Waypoint currentWaypoint;
+
+		private int direction;
+
+		private void Awake() => controller = GetComponent<EnemyWaypointNavigationController>();
+
+		private void Start()
+		{
+			direction = Mathf.RoundToInt(Random.Range(0f, 1f));
+			controller.SetDestination(currentWaypoint.GetPosition());
+		}
+
+		private void Update()
+		{
+			if (controller.ReachedDestination)
+			{
+				var shouldBranch = false;
+
+				if (currentWaypoint.Branches.Count > 0)
+					shouldBranch = Random.Range(0f, 1f) <= currentWaypoint.BranchRatio;
+
+				if (shouldBranch)
+				{
+					var rnd = Mathf.RoundToInt(Random.Range(0f, currentWaypoint.Branches.Count - 1f));
+					currentWaypoint = currentWaypoint.Branches[rnd];
+				}
+				else
+				{
+					switch (direction)
+					{
+						case 0 when currentWaypoint.NextWaypoint != null:
+							currentWaypoint = currentWaypoint.NextWaypoint;
+							break;
+						case 0:
+							currentWaypoint = currentWaypoint.PreviousWaypoint;
+							direction = 1;
+							break;
+						case 1 when currentWaypoint.PreviousWaypoint != null:
+							currentWaypoint = currentWaypoint.PreviousWaypoint;
+							break;
+						case 1:
+							currentWaypoint = currentWaypoint.NextWaypoint;
+							direction = 0;
+							break;
+					}
+				}
+				
+				controller.SetDestination(currentWaypoint.GetPosition());
+			}
+		}
+
+		public void SetCurrentWaypoint(Waypoint waypoint) => currentWaypoint = waypoint;
+	}
+}
